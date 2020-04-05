844a363ea9fd9bcb5ab13dc70ce270ff57278c3d
Vignesh Chandran <vigneshkc.mech@gmail.com>
2020-04-05 09:39:23 -0400

    Error Scenario changes

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', e => {
     e.preventDefault();
     fetch('/weather?address=' + input.value).then(res => {
         res.json().then(data => {
-            address.textContent = data.location;
+            address.textContent = data.location || data.error;
             weather.textContent = data.forecast
         })
     })
